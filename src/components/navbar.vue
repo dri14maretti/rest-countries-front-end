703abd4c43a70bf040f2f0ea8f28e4e08c3dfa01
unknown <dri.cmaretti@gmail.com>
2021-08-23 12:48:14 -0300

    adicionando vue material

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-	<nav class="navbar navbar-light bg-light">
+	<nav class="navbar navbar-light bg-light md-elevation-3">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="#">
 				<img src="../assets/img/footer_mova.svg" width="60">
